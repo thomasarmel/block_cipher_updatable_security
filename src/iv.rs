33ca8238a8b61490a6347deeca73b6c5c9ad0bf7
Thomas Prévost <thomas.prevost@univ-cotedazur.fr>
2025-04-10 17:29:15 +0200

    Add IV cache for faster encryption

--- src/iv.rs
+++ src/iv.rs
@@ -1,25 +1,36 @@
+use std::cell::RefCell;
+use std::collections::HashMap;
 use crate::POLYNOMIAL_Q;
-use crate::polynomial_algebra::poly_pow_mod;
+use crate::polynomial_algebra::{poly_pow_mod, polymul_fast};
 use crate::utils::gen_uniform_poly;
 use polynomial_ring::Polynomial;
 
 pub struct Iv {
     polynomial: Polynomial<i64>,
     initial_security: usize,
+    pow_cache: RefCell<HashMap<usize, Vec<Polynomial<i64>>>>,
 }
 
 impl Iv {
     pub fn generate(initial_security: usize) -> Self {
+        let polynomial = gen_uniform_poly(initial_security, POLYNOMIAL_Q as i64, None);
+        let mut cache = HashMap::new();
+        cache.insert(initial_security, vec![polynomial.clone()]);
         Self {
-            polynomial: gen_uniform_poly(initial_security, POLYNOMIAL_Q as i64, None),
+            polynomial: polynomial.clone(),
             initial_security,
+            pow_cache: RefCell::new(cache),
         }
     }
 
-    pub fn from_polynomial(polynomial: Polynomial<i64>, initial_security: usize) -> Self { // TODO remove
+    #[allow(dead_code)]
+    pub(crate) fn from_polynomial(polynomial: Polynomial<i64>, initial_security: usize) -> Self {
+        let mut cache = HashMap::new();
+        cache.insert(initial_security, vec![polynomial.clone()]);
         Self {
-            polynomial,
+            polynomial: polynomial.clone(),
             initial_security,
+            pow_cache: RefCell::new(cache),
         }
     }
 
@@ -32,6 +43,20 @@ impl Iv {
     }
 
     pub(crate) fn pow(&self, exponent: usize, modulo: &Polynomial<i64>) -> Polynomial<i64> {
+        let index = exponent - 1;
+        let cache_position = modulo.deg().unwrap();
+        if !self.pow_cache.borrow().contains_key(&cache_position) {
+            self.pow_cache.borrow_mut().insert(cache_position, vec![self.polynomial.clone()]);
+        }
+        let cache_len = self.pow_cache.borrow().get(&cache_position).unwrap().len();
+        if cache_len == index {
+            let next = polymul_fast(&self.polynomial, &self.pow_cache.borrow().get(&cache_position).unwrap()[index - 1], POLYNOMIAL_Q as i64, modulo);
+            self.pow_cache.borrow_mut().get_mut(&cache_position).unwrap().push(next.clone());
+            return next;
+        }
+        if cache_len > index {
+            return self.pow_cache.borrow().get(&cache_position).unwrap()[index].clone();
+        }
         poly_pow_mod(
             &self.polynomial,
             exponent,
